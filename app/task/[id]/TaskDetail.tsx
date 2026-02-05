eb92a468001125ca0d0dbd904c07258dbdfb2d3d
tshonosuke <t.showno.zzz@gmail.com>
2026-02-05 15:58:29 +0900

    タスク詳細ページ作成

---
+++ app/task/[id]/TaskDetail.tsx
@@ -0,0 +1,18 @@
+
+import { Task } from "@/app/components/ParentTaskList";
+import react from "react";
+
+
+const TaskDetail = ({task}: {task: Task}) => {
+
+  return (
+    <div>
+      <div>
+        
+      </div>
+    </div>
+  );
+
+}
+
+export default TaskDetail;
